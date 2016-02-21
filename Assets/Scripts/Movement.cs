1b75647fcd5b2f0db293b34c8233c37bee2227ad
David <elchicoconinsomnio@gmail.com>
2016-02-21 09:37:18 +0100

    Movement and blocks prototype

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+    public float m_RotationSpeed = 10f;
+    public float m_MovementSpeed = 10f;
+
+    public void Rotate (Vector3 pRotation)
+    {
+        transform.Rotate(pRotation * Time.deltaTime * m_RotationSpeed);
+    }
+
+    public void Move(Vector3 pMovement)
+    {
+        transform.Translate(pMovement * Time.deltaTime * m_MovementSpeed);
+    }
+}
