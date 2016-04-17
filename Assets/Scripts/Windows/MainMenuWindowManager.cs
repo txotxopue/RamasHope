4fbaca136ebf4bcb6665c17e2b6a9f8363b6fc81
David <elchicoconinsomnio@gmail.com>
2016-04-17 21:59:22 +0200

    main menu and game linked. Also obstacle spawning fixes

--- Assets/Scripts/Windows/MainMenuWindowManager.cs
+++ Assets/Scripts/Windows/MainMenuWindowManager.cs
@@ -1,7 +1,21 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MainMenuWindowManager : MonoBehaviour
 {
+    [SerializeField]
+    private string sceneName = "test";
 
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
 }
