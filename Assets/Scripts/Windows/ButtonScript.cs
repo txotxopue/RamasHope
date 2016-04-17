fbf565fd5d61099d12c5f050e8177d97d808ab43
David <elchicoconinsomnio@gmail.com>
2016-04-17 20:31:02 +0200

    text font and arrow buttons

---
+++ Assets/Scripts/Windows/ButtonScript.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, ISelectHandler, IDeselectHandler
+{
+    [SerializeField]
+    private Image selectedImage;
+
+    public void OnPointerEnter (PointerEventData eventData)
+    {
+        selectedImage.enabled = true;
+    }
+
+    public void OnPointerExit (PointerEventData eventData)
+    {
+        selectedImage.enabled = false;
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        selectedImage.enabled = true;
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        selectedImage.enabled = false;
+    }
+}
