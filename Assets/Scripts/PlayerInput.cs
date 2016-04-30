84e1b2f45aee01513a4fe80b9d0392c907690385
Razgriz85 <elchicoconinsomnio@gmail.com>
2016-05-01 01:32:31 +0200

    arrow buttons on game over screen

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,33 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// This Component captures uses input and passes it along
+/// to the appropiate player component.
+/// </summary>
 [RequireComponent (typeof(Movement))]
 public class PlayerInput : MonoBehaviour
 {
+    ///<summary> Component that handles the movement issued by the input captured here </summary>
+    [SerializeField]
     private Movement m_PlayerMovement;
+    /// <summary> Amount of horizontal axis captured</summary>
     private Vector3 rotationAmount = Vector3.zero;
-    private Vector3 movementAmount = Vector3.zero;
 
-	// Use this for initialization
-	void Start ()
-    {
-        m_PlayerMovement = GetComponent<Movement>();
-	}
 	
-	// Update is called once per frame
 	void FixedUpdate ()
     {
         rotationAmount.z = Input.GetAxis("Horizontal");
-        movementAmount.z = Input.GetAxis("Movement");
         if (rotationAmount != Vector3.zero)
         {
             m_PlayerMovement.Rotate(rotationAmount);
         }
-        /* Disabled!
-        if (movementAmount != Vector3.zero)
-        {
-            m_PlayerMovement.Move(movementAmount);
-        }
-        */
 	}
 }
